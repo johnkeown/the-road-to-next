e501b4b5e791d05c9aacdc6b92f2f65e91c72495
John Keown <jkeown@pplweb.com>
2025-10-24 14:21:21 -0400

    completed Fallback Routes section

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,38 +1,26 @@
-import Link from "next/link";
-import Placeholder from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
+import { notFound } from "next/navigation";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
-import { ticketsPath } from "@/path";
 
 type TicketPageProps = {
-    params: Promise<{
-        ticketId: string;
-    }>;
+  params: Promise<{
+    ticketId: string;
+  }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-    const { ticketId } = await params;
-    const ticket = await getTicket(ticketId);
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId);
 
-    if (!ticket) {
-        return (
-            <Placeholder
-                label="Ticket not found"
-                button={
-                    <Button asChild variant="outline">
-                        <Link href={ticketsPath()}>Go to Tickets</Link>
-                    </Button>
-                }
-            />
-        );
-    }
+  if (!ticket) {
+    notFound();
+  }
 
-    return (
-        <div className="flex justify-center animate-fade-from-top">
-            <TicketItem ticket={ticket} isDetail={true} />
-        </div>
-    );
+  return (
+    <div className="flex justify-center animate-fade-from-top">
+      <TicketItem ticket={ticket} isDetail={true} />
+    </div>
+  );
 };
 
 export default TicketPage;
